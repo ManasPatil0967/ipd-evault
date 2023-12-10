e1b6ae0106ec46754c7ea06a57ad12dddfc38292
ManasPaitl0967 <manaspatil0967@gmail.com>
2023-12-10 19:58:05 +0530

    Home

--- dashboard/pages/index.js
+++ dashboard/pages/index.js
@@ -1,147 +1,59 @@
-import React from 'react';
-import Image from 'next/image'
-import Head from 'next/head';
-import styled from 'styled-components'
-import Link from 'next/link';
-import Component from './login';
-
-
-export const StyledHeading = styled.h1`
-  font-size: 10rem;
-  background: -webkit-gradient(
-    linear,
-    left top,
-    right top,
-    from(#FF00FA),
-    to(#95CDFF)
-  );
-  background: linear-gradient(175deg,#00C2FF,#FF00FA);
-  -webkit-background-clip: text;
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
-  -webkit-box-decoration-break: clone;
-  box-decoration-break: clone;
-  text-shadow: none;
-`;
-
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/jbrmrkXHLX7
+ */
 export default function Home() {
-
   return (
-    <main>
-
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Outfit&display=swap" rel="stylesheet"/>
-      </Head>
-      <section className='buttons'>
-        <div className='button-container'>
-          <div className='right-regi-btns'>
-              <Link href="/schemes">
-                <div className='schemes' > View Schemes </div>
-              </Link>
-          </div>
-            <div className='left-regi-btns'>
-            <Link href="/signup">
-              <div className='signUp' > Sign Up </div>
-            </Link>
-            <Link href="/login">
-              <div className='logIn'> Log In </div>
-            </Link>  
-            </div>
-        </div>    
-          
-      <style jsx>{`
-
-        .button-container{
-          display: flex;
-          justify-content: flex-start;
-        }
-
-        .right-regi-btns{
-          padding-right: 70rem;
-          justify-self: center;
-          text-align: center;
-        }
-        .schemes{
-          padding:0.6rem 2rem;
-          margin-top: 10px;
-          border-radius: 10px;
-          border: solid white 1px;
-          
-        }
-        .left-regi-btns{
-            display:flex;
-            justify-content: flex-end;
-            justify-self: center;
-            text-align: center;            
-          }
-        .signUp{
-          padding:0.6rem 2rem;
-          margin-top: 10px;
-          border-radius: 10px;
-          border: solid white 1px;
-        }
-        .logIn{        
-          padding:0.6rem 2rem;
-          margin-top: 10px;
-          border-radius: 10px;
-          border: solid white 1px;
-          margin-left: 10px;
-          margin-right: 10px;
-        }            
-        `}</style>
-
-      </section>
-      <section className='hero-section'>
-        <div className='hero-container'>
-          <div className='title'>
-            <StyledHeading>E-Vault</StyledHeading>
-          </div>
-          <div className='subtitle'>
-            <h2>Get Your Files Secured</h2>
-          </div>
-          <div className='subtext'>
-            <p>Our technology performing fast blockchain (120K TPS) and it has guaranteed AI-based data security. Proof of Stake, its consensus algorithm enables unlimited speeds.</p>
-          </div>
+    <div className="h-screen flex flex-col bg-[#22c55e]">
+      <nav className="bg-black flex items-center justify-between px-6 py-4">
+        <div className="flex items-center">
+          <img alt="Vercel logo" className="w-8 h-8 mr-3" src="/placeholder.svg" />
+          <h1 className="text-2xl font-bold text-white">DocVault</h1>
         </div>
-
-        <style jsx>{`
-          .hero-container{
-            font-family: 'Outfit', sans-serif;  
-            display:flex;
-            justify-content:center;
-            align-items:center;
-            flex-direction:column;
-            padding-top: 100px;            
-          }
-          .title{
-            padding-top: 1.5rem;
-            padding-bottom: 0;
-            font-size: 10rem;
-            z-index: -4;
-          }
-          .subtitle{
-            padding:0;
-            margin-bottom: 30px;
-            font-size: 5rem;
-            z-index:-4;
-          }
-          .subtext{
-            padding-top: 1.6rem;
-            font-size: 1.2rem;
-            font-weight: 300;
-          }
-          
-        `}</style>
-      </section>
-      <section className='bottom'>
-        <div className='looperImg'>
-          <div className="opacity_div"></div>
+        <div className="flex items-center space-x-4">
+          <a className="text-xl text-white" href="#">
+            Signup
+          </a>
+          <a className="text-xl text-white" href="#">
+            Login
+          </a>
         </div>
-      </section>
-      
-    </main>
+      </nav>
+      <div className="border-t border-white" />
+      <div className="flex-grow flex flex-col items-center justify-center bg-black text-white">
+        <h2 className="text-5xl font-bold">Welcome to DocVault</h2>
+        <p className="text-xl mt-6 px-4 text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non est id velit porttitor malesuada. Cras
+          lacinia risus at dui pellentesque, sed finibus lorem condimentum.
+        </p>
+      </div>
+      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 hidden">
+        <form className="w-full max-w-lg p-6 mt-6 bg-white text-black">
+          <h2 className="text-2xl font-bold mb-6 text-center">Request Certificate</h2>
+          <div className="flex flex-wrap mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="certificate-name">
+              Certificate Name:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="certificate-name"
+              placeholder="Certificate Name"
+              required
+              type="text"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-[#22c55e] hover:bg-[#16a34a] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
+
 
