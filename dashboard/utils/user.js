9dcba48c81c6ea858f8b86e81f59412e2de42c3d
ManasPatil0967 <manaspatil0967@gmail.com>
2023-12-12 22:40:53 +0530

    Changes 12/12

--- dashboard/utils/user.js
+++ dashboard/utils/user.js
@@ -2,10 +2,11 @@ import { ethers } from "ethers";
 import abi from "./abi.json";
 
 const contractAddress = "0xd9145CCE52D386f254917e481eB44e9943F39138";
-
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-const contract = new ethers.Contract(contractAddress, abi, provider);
-
+let provider, contract;
+if (typeof window.ethereum !== 'undefined') {
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+    contract = new ethers.Contract(contractAddress, abi, provider);
+}
 const createUser = async (name, age, income, state) => {
     const signer = provider.getSigner();
     const contractWithSigner = contract.connect(signer);
